a9c0615f631b3edffa40f0ef396e880da61bd501
R Vickramapandian <r.vickramapandian@dgsl.in>
2024-02-13 20:57:00 +0530

    push notifications

--- backend/modules/django_push_notifications/push_notifications/serializers.py
+++ backend/modules/django_push_notifications/push_notifications/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import OneSignalApp
+#from .models import OneSignalApp
 
-class OneSignalAppSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OneSignalApp
-        fields = '__all__'
+# class OneSignalAppSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = OneSignalApp
+#         fields = '__all__'
+
+class NotificationSerializer(serializers.Serializer):
+    app_id = serializers.CharField(max_length=255)
+    rest_api_key = serializers.CharField(max_length=255)
+    included_segments = serializers.ListField(child=serializers.CharField(max_length=255))
+    data = serializers.DictField(child=serializers.CharField(max_length=255))
+    contents = serializers.DictField(child=serializers.CharField(max_length=255))
