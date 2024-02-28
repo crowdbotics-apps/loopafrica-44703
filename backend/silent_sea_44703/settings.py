b59749e5677fea866ecf5ef3daffcd73cf482222
R Vickramapandian <r.vickramapandian@dgsl.in>
2024-02-28 20:33:06 +0530

    AWS - settings updated

--- backend/silent_sea_44703/settings.py
+++ backend/silent_sea_44703/settings.py
@@ -276,6 +276,9 @@ if USE_S3:
         "DEFAULT_FILE_STORAGE", "home.storage_backends.MediaStorage"
     )
 
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_QUERYSTRING_AUTH = False
+
 SPECTACULAR_SETTINGS = {
     # available SwaggerUI configuration parameters
     # https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
