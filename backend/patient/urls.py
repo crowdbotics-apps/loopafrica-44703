d8efab690b022672a578042d5989e4303b93b690
R Vickramapandian <r.vickramapandian@dgsl.in>
2024-03-07 18:51:47 +0530

    Test results and med record

--- backend/patient/urls.py
+++ backend/patient/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import PatientListCreateView, PatientRetrieveUpdateDestroyView
 from rest_framework.routers import DefaultRouter
 #from .views import VitalsViewSet
-from .viewsets import VitalsViewSet, PrescriptionViewSet, MedicalRecordViewSet
+from .viewsets import VitalsViewSet, PrescriptionViewSet, MedicalRecordViewSet, TestResultViewSet, TestResultUploadViewSet
 
 app_name = 'patient_profile'
  
@@ -10,7 +10,8 @@ router = DefaultRouter()
 router.register(r'vitals', VitalsViewSet)
 router.register(r'prescriptions', PrescriptionViewSet)
 router.register(r'medical_records', MedicalRecordViewSet)
-
+router.register(r'test-results', TestResultViewSet)
+router.register(r'test-results-upload', TestResultUploadViewSet, basename='test-result-upload')
 
 urlpatterns = [
     path('', include(router.urls)),
