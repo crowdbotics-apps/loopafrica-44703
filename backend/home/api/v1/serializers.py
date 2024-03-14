cfd450e09208f38eaee359ea8faf8b7a452c63bd
R Vickramapandian <r.vickramapandian@dgsl.in>
2024-03-14 16:07:29 +0530

    search sort filter and pagination for doctor API

--- backend/home/api/v1/serializers.py
+++ backend/home/api/v1/serializers.py
@@ -117,7 +117,7 @@ class SignupWithEmailSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'dob','full_name','phone_number', 'gender', 'profile_picture']
+        fields = ['id', 'email', 'full_name', 'first_name', 'last_name', 'dob','full_name','phone_number', 'gender', 'profile_picture']
 
 class DoctorListSerializer(serializers.ModelSerializer):    
     class Meta:
